2e8d51613fdf8a785e3dbe5dd940514e9198e752
Sedefnur Kayran <109733884+sedefnurkayran@users.noreply.github.com>
2025-10-02 08:01:51 +0200

    The work has been completed up to the Monitoring/Reporting stage

--- NumPyCase/numpyCase.py
+++ NumPyCase/numpyCase.py
@@ -86,13 +86,63 @@ print(sd[1,1,:]) #2. block, 2. rows
 
 
 # Fency Indexleme
-
-
 # arr = np.array([10,20,30,40,50])
 # idx = [0,2,4]
 # print(zd[idx])
 
 #Brodcasting
+#NumPy şekilleri (shape) sağdan hizalar. 
+# Eğer sütun sayısı eşitse → satırlara yayılır (satır-bazlı)
+# Eğer satır sayısı eşitse → sütunlara yayılır (sütun-bazlı)
+
 # A = np.ones((3,4))
 # b = np.array([1,2,3,4])
 # print(A+b)
+
+#Brodcasting with Multi-Dimensional Arrays (column-wise)
+# array1 = np.array([[1, 2, 3], [4, 5, 6]])   # shape = (2,3)
+# array2 = np.array([[10], [20]]) # shape = (2,1) 2 rows 1 column
+# result = array1 + array2
+# print(result)
+
+print("Sedef")
+#Broadcasting with Different Dimensions (row-wise)
+bc_a = np.array([[1, 2, 3], [4, 5, 6]]) #shape = (2,3) 2 rows 3 columns
+bc_b = np.array([10, 20, 30])  # shape = (3,) 1 row 3 columns
+result = bc_a + bc_b
+print(result)
+
+# Row-based z-normalization (Bu bölümde her satır, kendi ortalama ve standart sapmasına göre standardize edilmiştir.)
+# Amaç: satır ortalamalarını ~0, satır std'lerini ~1 yapmak.
+row_mean = bc_a.mean(axis=1, keepdims=True)  #tek satırlık bir vektör, normalizasyon için kullanılmaz.
+row_std = bc_a.std(axis=1, keepdims=True) 
+row_std_safe = np.where(row_std == 0, 1, row_std)
+Z = (bc_a - row_mean) / row_std_safe
+print(Z)
+
+#Jupyter Notebook
+print("Original Data:\n",bc_a)
+print("Global Mean:", bc_a.mean())
+print("Global Std:", bc_a.std())
+
+print("Row Mean:\n" , row_mean)
+print("Row Std:\n" , row_std)
+print("Row Safe Std:\n" , row_std_safe)
+
+print("Normalized Result:\n", Z)
+print("Row means after normalization:\n", Z.mean(axis=1))
+print("Row stds after normalization:\n", Z.std(axis=1))
+
+summary = np.column_stack([
+    row_mean.ravel(), 
+    row_std.ravel(), 
+    Z.mean(axis=1), 
+    Z.std(axis=1)])
+print("Rows: [Bef_Means, Bef_Stds, Aft_Means, Aft_Stds]\n", summary)
+# print("Rows: [Bef.N. Means, Bef.N. Stds, Aft. N. Means, Aft.N. Stds]\n", np.round(summary,6))
+
+## Önce / Sonra Karşılaştırma 
+#Normalizasyon öncesinde satır ortalamaları ve standart sapmaları farklıydı (bkz. “Row means/stds (before)”). 
+#Broadcasting ile satır bazlı z-normalizasyon uygulandı: her satırdan kendi ortalaması çıkarılıp kendi std’sine bölündü. 
+#İşlem sonrası her satırın ortalaması ≈ 0 ve std’si ≈ 1 oldu (bkz. “Row means/stds (after)” ve otomatik doğrulama). 
+#Böylece tüm satırlar aynı ölçekte karşılaştırılabilir hale geldi.
